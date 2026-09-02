ddf2af0fc44576e976ca9e6a48944d6910f0ff2a
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-02 20:37:00 +0530

    fix: mount stable online status layer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import ProfilePageV4 from "../components/ProfilePageV4";
 import ProfileEnhancements from "../components/ProfileEnhancements";
+import AlwaysOnline from "../components/AlwaysOnline";
 import "../components/profile-enhancements.css";
-export default function Home(){return <><ProfilePageV4/><ProfileEnhancements/></>}
+export default function Home(){return <><ProfilePageV4/><ProfileEnhancements/><AlwaysOnline/></>}
