dce55e49678a7dbec6f650b0be91a4fd57cc8282
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-02 20:36:11 +0530

    style: load final profile polish layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "../components/profile-upgrades.css";
 import "../components/profile-v5.css";
 import "../components/profile-v6.css";
 import "../components/profile-super.css";
+import "../components/profile-final.css";
 const title=process.env.NEXT_PUBLIC_SITE_TITLE||process.env.NEXT_PUBLIC_NAME||"Profile";
 const description=process.env.NEXT_PUBLIC_SITE_DESCRIPTION||process.env.NEXT_PUBLIC_BIO||"Personal profile website.";
 export const metadata:Metadata={title,description,icons:{icon:"/favicon.svg"}};
