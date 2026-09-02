0c669445d0b2bc7ae6af146a2e600bd2510e5990
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-02 20:48:54 +0530

    style: mount premium profile wow layer

--- app/page.tsx
+++ app/page.tsx
@@ -2,4 +2,5 @@ import ProfilePageV4 from "../components/ProfilePageV4";
 import ProfileEnhancements from "../components/ProfileEnhancements";
 import AlwaysOnline from "../components/AlwaysOnline";
 import "../components/profile-enhancements.css";
+import "../components/profile-wow.css";
 export default function Home(){return <><ProfilePageV4/><ProfileEnhancements/><AlwaysOnline/></>}
