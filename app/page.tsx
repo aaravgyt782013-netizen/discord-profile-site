007dd5d59725771cc7d9aa23fc6277f40ea83520
aaravgyt782013-netizen <aaravgyt782013@gmail.com>
2026-09-03 14:22:41 +0530

    feat: add upload platform tabs to profile

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import ProfilePageV4 from "../components/ProfilePageV4";
 import ProfileEnhancements from "../components/ProfileEnhancements";
 import AlwaysOnline from "../components/AlwaysOnline";
 import ProfileExtras from "../components/ProfileExtras";
+import ProfileUploadsTabs from "../components/ProfileUploadsTabs";
 import "../components/profile-enhancements.css";
 import "../components/profile-wow.css";
 import "../components/profile-badge-polish.css";
 import "../components/profile-publish.css";
 // Keep the original profile design intact; the new feature layer is additive only.
-export default function Home(){return <><ProfilePageV4/><ProfileExtras/><ProfileEnhancements/><AlwaysOnline/></>}
+export default function Home(){return <><ProfilePageV4/><ProfileExtras/><ProfileUploadsTabs/><ProfileEnhancements/><AlwaysOnline/></>}
